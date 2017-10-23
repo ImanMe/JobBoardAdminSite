8e35258ec77cf5c353a21b470ca5aaa8282b85f3
iman memarpour <iman.memarpour@gmail.com>
2017-10-22 21:41:46 -0400

    chnages after migration from Azure

--- src/app/services/board/board.service.ts
+++ src/app/services/board/board.service.ts
@@ -5,7 +5,8 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class BoardService {
 
-  private endpoint = 'http://jobboardadminapi20170917094805.azurewebsites.net/api/';
+  //private endpoint = 'http://jobboardadminapi20170917094805.azurewebsites.net/api/';
+  private endpoint = 'http://localhost:51076/api/';
   constructor(private http: Http) { }
 
   getBoards() {
